4b158dfc2f13f5dbef0d07cc27f59c3b0c2851ed
chiyu <2270588502@qq.com>
2024-03-12 16:56:36 +0800

    增加naive-protable的配置文件

---
+++ packages/naive-protable/vite.config.ts
@@ -0,0 +1,45 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
+import { resolve } from "path";
+import dts from "vite-plugin-dts";
+export default defineConfig({
+	plugins: [
+		dts({
+			copyDtsFiles: true,
+			outDir: [
+				"dist",
+			],
+			clearPureImport: false,
+			include: ["src/*", "src/types.ts"],
+			insertTypesEntry: true,
+			compilerOptions: {
+			},
+		}),
+		vueJsx(),
+		vue(),
+	],
+	build: {
+		lib: {
+			// Could also be a dictionary or array of multiple entry points
+			entry: resolve(__dirname, "src/index.ts"),
+			name: "@suite-kit/n-protable",
+			// the proper extensions will be added
+			fileName: "index",
+		},
+		rollupOptions: {
+			// // 确保外部化处理那些你不想打包进库的依赖
+			external: ["vue", "naive-ui"],
+			output: {
+				// 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
+				globals: {
+					vue: "Vue",
+					"naive-ui": "naive-ui",
+				},
+			},
+		},
+	},
+	optimizeDeps: {
+		include: [resolve(__dirname, "src/style.scss")],
+	},
+});
